bafaa2ebd792925b9ac2dba0e1f7af3abe361923
galihaliifah27 <alifahtyas2@gmail.com>
2025-05-13 13:07:27 +0700

    update

---
+++ application/views/layout/footer.php
@@ -0,0 +1,27 @@
+<footer class="main-footer text-center">
+    <strong>Copyright &copy;
+        <?=date('Y'); ?> Portal Berita</strong>
+</footer>
+<aside class="control-sidebar control-sidebar-dark">
+</aside>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.18/summernote-bs4.min.js"></script>
+<script src="<?= base_url('assets/adminlte/plugins/jquery-ui/jquery-ui.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/chart.js/chart.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/sparklines/sparkline.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/jqvmap/jquery.vmap.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/jquery-knob/jquery.knob.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/moment/moment.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/daterangepicker/daterangepicker.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/overlayScrollBars/js/jquery.overlayScrollBars.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/dist/js/pages/dashboard.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js')?>"></script>
+<script src="<?= base_url('assets/adminlte/dist/js/demo.js')?>"></script>
+</body>
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Blank Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/fontawesome-free/css/all.min.css');?>">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.css');?>">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css');?>">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/jqvmap/jqvmap.min.css');?>">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/overlayScrollBars/css/OverlayScrollBars.min.css');?>">
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/daterangepicker/daterangepicker.css');?>">
+  
+  <!-- Summernote CSS (Make sure to use only one link) -->
+  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+  
+  <!-- Bootstrap CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  
+  <!-- AdminLTE Theme style -->
+  <link rel="stylesheet" href="<?= base_url('assets/adminlte/dist/css/adminlte.min.css');?>">
+  
+  <style>
+    .center-text {
+      text-align: center;
+      width: 100%;
+    }
+  </style>
+</head>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Portal Berita</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breakcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Layout</a></li>
+                        <li class="breadcrumb-item"><a href="#">Boxed Layout</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
